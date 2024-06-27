14c56a0cafbd1e3b6278542015204b73510fb0f7
yeongbee <handfaste@naver.com>
2024-06-27 20:35:17 +0900

    blog post word

--- src/main/java/com/yeongbee/store/mydelight/blog/service/BlogService.java
+++ src/main/java/com/yeongbee/store/mydelight/blog/service/BlogService.java
@@ -8,6 +8,7 @@ import com.yeongbee.store.mydelight.blog.domain.blog.UploadFile;
 import com.yeongbee.store.mydelight.blog.repository.BlogRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -20,9 +21,9 @@ import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BlogService {
 
-    private static final Logger log = LoggerFactory.getLogger(BlogService.class);
     private final BlogRepository blogRepository;
     private final FIleStore fIleStore;
 
