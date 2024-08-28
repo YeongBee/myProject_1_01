407bce1a2aff47fd534cd4aaef8f33a6119a3d5a
yeongbeePC <handfaste@naver.com>
2024-08-28 12:19:57 +0900

    Discord Webpage Sample2

--- src/main/resources/static/discord/discord.js
+++ src/main/resources/static/discord/discord.js
@@ -13,13 +13,13 @@ const posts = [
             "                4. !명령삭제 를 사용하여  저장된 명령어를 삭제할 수 있습니다.\n (ex : !명령삭제 안녕 )\n\n" +
             "                5. !인장 을 사용하여  인장주작하는 을 볼 수 있습니다.\n (ex : !인장 )\n\n" +
             "                6. !메이플 을 사용하여 캐릭터의 정보를 볼 수 있습니다.\n (!명령어 (캐릭터이름) )\n                (ex : !메이플 아델 )\n\n" +
-            "                7. !스타포스 를 사용하여 계정의 스타포스에 사용한 비용을 확인할 수 있습니다.\n 자신의 API가 필요합니다. API는 한번 저장하면 영구적으로 저장 가능합니다.\n ( ex : !스타포스 ) ( ex : !api저장 live_apikey.... )\n ( ex : !api삭제 )\n\n" +
+            "                7. !스타포스 를 사용하여 계정의 스타포스에 사용한 비용을 확인할 수 있습니다.\n 자신의 API가 필요합니다. API는 한번 저장하면 영구적으로 저장 가능합니다.\n ( ex : !스타포스 )\n ( ex : !api저장 live_apikey.... )\n ( ex : !api삭제 )\n\n" +
             "                8. !잠재능력 을 사용하여 계정의 잠재능력에 사용한 비용을 알 수 있습니다. \n자신의 API가 필요합니다. \n( ex : !잠재능력)\n\n"+
             "                9. !큐브 를 사용하여 사용한 큐브의 종류와 개수를 알 수 있습니다. \n자신의 API가 필요합니다. \n (ex : !큐브)"
     },
-    {title: "제작자", content: "제작자 : Yeongbee \n\n Email : cyeongbee.gmail.com"}
-];
+    {title: "제작자", content: "제작자 : Yeongbee \n\n Email : cyeongbee@gmail.com"}
 
+];
 
 function showContent(index, element) {
     const contentArea = document.getElementById('contentArea');
